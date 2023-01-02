219a9ca71ea822e6ad22d580b3b3d9bbf97c7dd4
Sergio <Sergio@DESKTOP-O35L3I8>
2023-01-02 12:36:19 +0100

    cambié las fuentes xd

--- src/main/java/interfaz/clasroom/AsignaturasController.java
+++ src/main/java/interfaz/clasroom/AsignaturasController.java
@@ -20,7 +20,7 @@ public class AsignaturasController implements Initializable {
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        
     }    
     
 }
